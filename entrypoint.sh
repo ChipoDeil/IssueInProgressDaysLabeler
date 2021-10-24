ba182d3d310334a357d7ccb341e2ff4411341eb9
chipodeil <bus.yaroslav@gmail.com>
2021-10-25 00:07:16 +0300

    action yml added

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,6 +6,6 @@ dotnet restore
 dotnet build
 dotnet run --project IssueInProgressDaysLabeler.Model -- \
     --owner "$owner" \
-    --repository-name "$repository-name" \
+    --repository-name "$repository_name" \
     --labels "$labels" \
-    --github-token "$github-token"
+    --github-token "$github_token"
