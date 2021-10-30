a20c9e8887a5ff46e1c1ce08a6b7f031c7d5135b
chipodeil <bus.yaroslav@gmail.com>
2021-10-30 17:04:09 +0300

    replace double quotes with single ones

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,7 +5,7 @@ cd /app
 dotnet restore
 dotnet build
 dotnet run --project IssueInProgressDaysLabeler.Model -- \
-    --owner "$owner" \
-    --repository-name "$repository_name" \
-    --labels "$labels" \
-    --github-token "$github_token"
+    --owner '$owner'\
+    --repository-name '$repository_name' \
+    --labels '$labels' \
+    --github-token '$github_token'
