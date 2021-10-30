11ee4b3764a4f36086e16c413b5d5c790377aa1f
chipodeil <bus.yaroslav@gmail.com>
2021-10-31 01:25:01 +0300

    cleanup

--- Dockerfile
+++ Dockerfile
@@ -2,7 +2,7 @@ FROM mcr.microsoft.com/dotnet/sdk:5.0 AS build
 WORKDIR /app
 
 LABEL "com.github.actions.name"="Issues tracking time in progress"
-LABEL "com.github.actions.description"="Visualize time issues spend in columns"
+LABEL "com.github.actions.description"="Visualize time issues spend in assigned state"
 
 LABEL "repository"="https://github.com/ChipoDeil/IssueInProgressDaysLabeler"
 
--- IssueInProgressDaysLabeler.Model/Options.cs
+++ IssueInProgressDaysLabeler.Model/Options.cs
@@ -1,40 +1,21 @@
-using System.Collections.Generic;
 using CommandLine;
 
 namespace IssueInProgressDaysLabeler.Model
 {
-    /// <summary>
-    /// This represents the parameters entity for the console app.
-    /// </summary>
     public class Options
     {
-        /// <summary>
-        /// Repository name and owner
-        /// </summary>
         [Option("github-repository", Required = true, HelpText = "Repository to increment days in and owner")]
         public virtual string GithubRepositoryName { get; set; }
 
-        /// <summary>
-        /// Days on which action will increment value
-        /// </summary>
         [Option("days-mode", Required = true, HelpText = "Enum: EveryDay, EveryDayExceptWeekend, RussianCalendar")]
         public virtual DaysMode DaysMode { get; set; }
 
-        /// <summary>
-        /// Suitable labels for issue days incrementation
-        /// </summary>
         [Option("labels", Required = true, HelpText = "Suitable labels for issue days incrementation")]
         public virtual string Labels { get; set; }
 
-        /// <summary>
-        /// Github secret token
-        /// </summary>
         [Option("github-token", Required = true, HelpText = "Github secret token")]
         public virtual string GithubToken { get; set; }
 
-        /// <summary>
-        ///
-        /// </summary>
         [Option("label-to-increment",
             Required = true,
             HelpText = @"Label that will be added to issue, for example ""In progress: {0} days"", placeholder is required")]
