12b57deb953b91e31d131dbd720423a971d2eee8
chipodeil <bus.yaroslav@gmail.com>
2021-11-01 01:53:06 +0300

    deleted dash dash

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,7 @@
 
 cd /app
 
-dotnet IssueInProgressDaysLabeler.Model.dll -- \
+dotnet IssueInProgressDaysLabeler.Model.dll \
     --github-repository "$GITHUB_REPOSITORY" \
     --days-mode "$days_mode" \
     --labels "$labels" \
